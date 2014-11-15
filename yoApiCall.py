c6f7c5c0d620f5718e81555dad5b805e6fef94ea
Mamadou Diallo <mamadou888@gmail.com>
2014-11-15 13:45:23 -0500

    added server in flask

--- yoApiCall.py
+++ yoApiCall.py
@@ -1,2 +1,5 @@
 import requests
-requests.post("http://api.justyo.co/yo/", data={'api_token': api_token, 'username': 'JOHNAPPLESEED'})
+
+request = requests.post("http://api.justyo.co/yo/", data={'api_token': '8857de65-b94d-45c9-b1d7-752432b7e7d8', 'username': 'SQUAD5'}).text
+
+print request
